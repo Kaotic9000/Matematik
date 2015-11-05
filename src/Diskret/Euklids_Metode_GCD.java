3e8be0b884f565f3c66bca0e631333a954dde4b8
magnusmisbat <magnusmisbat@gmail.com>
2015-11-05 09:56:22 +0100

    Tilføjede Euklids Metode

---
+++ src/Diskret/Euklids_Metode_GCD.java
@@ -0,0 +1,29 @@
+package Diskret;
+
+public class Euklids_Metode_GCD {
+
+	public static void main(String[] args) {
+
+		int a = 188;
+		int b = 158;
+		int z;
+
+		if (b > a) {
+			z = b;
+			b = a;
+			a = z;
+		}
+
+		System.out.println("Vi benytter Euklids algoritme for at finde GCD" + '\n');
+		while (b != 0) {
+
+			int x = a % b;
+			System.out.println("a % b = x = " + x);
+			System.out.println("Vores a værdi til at være lig b, derefter sættes b til at være lig a % b");
+
+			a = b;
+			b = x;
+		}
+		System.out.println('\n' + "GCD af (a,b) = " + a);
+	}
+}
